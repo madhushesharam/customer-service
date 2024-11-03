2de2415d6b4834828af2596255b4620847b410a0
Madhu Shesharam <madhu@Madhus-MacBook-Pro.local>
2024-11-03 14:03:03 -0700

    Add base crud apis and relevant tests

--- test_app.py
+++ test_app.py
@@ -12,3 +12,19 @@ def client():
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
+
+
+# def test_create_customer(client):
+#     response = client.post("/customer")
+#     # TODO: Add request body
+#     assert response.status_code == 201
+
+
+def test_get_customer(client):
+    response = client.get("/customer/1234")
+    assert response.status_code == 200
+
+
+def test_delete_customer(client):
+    response = client.delete("/customer/1234")
+    assert response.status_code == 200
